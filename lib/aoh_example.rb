7e597679c69adbb13c0687a46cd1cb89aac90821
Jordan Dains <dains.jordan2@gmail.com>
2020-09-18 00:16:12 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -25,14 +25,14 @@ def literal_aoh
 end
 
 def aoh_lookup(aoh, row, key)
-  literal_array = [
+  aoh = [
     {name=>  "Don Gately", occupation=> "Live-in Staff"},
     {name=> "Joelle van Dyne", occupation=> "Radio Personality"},
     {name=> "Pat Monteseian", occupation=> "Staff"},
     {name=> "Kate Gompert", occupation=> "None"},
     {name=> "Bruce Green", occupation=> "Fan of Mildred"}
     ]
-    literal_array[0][:name]
+    literal_array[row][key]
 end
 
 def aoh_update(aoh, row, key, new_value)
