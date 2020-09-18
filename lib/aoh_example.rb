fedea7160ac65f370d839af12d49d3a46d574981
Jordan Dains <dains.jordan2@gmail.com>
2020-09-18 00:15:30 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -16,15 +16,23 @@ def literal_aoh
   # Using Array literal syntax only, build a nested array that uses the data in
   # held in the constants
   literal_array = [
-    {name =>  "Don Gately", occupation => "Live-in Staff"},
-    {name => "Joelle van Dyne", occupation => "Radio Personality"},
-    {name => "Pat Monteseian", occupation => "Staff"},
-    {name => "Kate Gompert", occupation => "None"},
-    {name => "Bruce Green", occupation => "Fan of Mildred"}
+    {name=>  "Don Gately", occupation=> "Live-in Staff"},
+    {name=> "Joelle van Dyne", occupation=> "Radio Personality"},
+    {name=> "Pat Monteseian", occupation=> "Staff"},
+    {name=> "Kate Gompert", occupation=> "None"},
+    {name=> "Bruce Green", occupation=> "Fan of Mildred"}
     ]
 end
 
 def aoh_lookup(aoh, row, key)
+  literal_array = [
+    {name=>  "Don Gately", occupation=> "Live-in Staff"},
+    {name=> "Joelle van Dyne", occupation=> "Radio Personality"},
+    {name=> "Pat Monteseian", occupation=> "Staff"},
+    {name=> "Kate Gompert", occupation=> "None"},
+    {name=> "Bruce Green", occupation=> "Fan of Mildred"}
+    ]
+    literal_array[0][:name]
 end
 
 def aoh_update(aoh, row, key, new_value)
